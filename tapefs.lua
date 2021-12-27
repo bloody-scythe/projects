59116b8d764d34bd9d95ddaabaa75960da634301
Bloody-Scythe <bloodyscythe404@gmail.com>
2021-12-27 05:14:15 -0300

    more fix

--- tapefs.lua
+++ tapefs.lua
@@ -6,12 +6,12 @@ fdb_size = 204800 -- size of the file descriptor block
 test_fdb = {}
 test_fdb['test.lua'] = { i = 204800, s = 50 }
 
-function tape.start()
-    return tape.seek(-tape.getPosition())
+function tape.go(pos)
+    return tape.seek(pos - tape.getPosition())
 end
 
 function read_fdb()
-    tape.start()
+    tape.go(0)
     size = tape.read(8):match('(.-)\0')
     fdb = tape.read(fdb_size):match('(.-)\0')
     if size == '' then size = 0 end
@@ -25,14 +25,15 @@ function read_fdb()
 end
 
 function write_fdb(fdb, previous_size)
-    tape.start()
+    tape.go(0)
     fdb = serial.serialize(fdb)
     size = #fdb
     if size > fdb_size then 
         error('error: File descriptor block too big!!')
     end
     tape.write(tostring(size))
-    tape.seek(9 - tape.getPosition())
+    tape.write(string.rep('\0', 8 - #tostring(size)))
     tape.write(tostring(fdb))
     a = test
 end
+
