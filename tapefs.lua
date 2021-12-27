858dbe1885ef09159d696991f6ce52538c4fa469
Bloody-Scythe <bloodyscythe404@gmail.com>
2021-12-27 04:44:46 -0300

    fix for test
    fix
    
    fix

--- tapefs.lua
+++ tapefs.lua
@@ -3,6 +3,8 @@ component = require('component')
 tape = component.tape_drive
 
 fdb_size = 204800 -- size of the file descriptor block
+test_fdb = {}
+test_fdb['test.lua'] = { i = 204800, s = 50 }
 
 function tape.start()
     return tape.seek(-tape.getPosition())
@@ -12,7 +14,6 @@ function read_fdb()
     tape.start()
     size = tape.read(8):match('(.-)\0')
     fdb = tape.read(fdb_size):match('(.-)\0')
-    tape.start()
     if size == '' then size = 0 end
     if fdb == '' then
         fdb = {}
@@ -26,12 +27,12 @@ end
 function write_fdb(fdb, previous_size)
     tape.start()
     fdb = serial.serialize(fdb)
-    size = tostring(#fdb)
+    size = #fdb
     if size > fdb_size then 
         error('error: File descriptor block too big!!')
     end
     tape.write(tostring(size))
     tape.seek(9 - tape.getPosition())
-    tape.write(fdb)
-    tape.seek(fdb_size - tape.getPosition())
+    tape.write(tostring(fdb))
+    a = test
 end
