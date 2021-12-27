a7ad7cf18903b108b5d2880e89ec8c11332a58e7
Bloody-Scythe <bloodyscythe404@gmail.com>
2021-12-27 07:10:15 -0300

    cmmit here

--- tapefs.lua
+++ tapefs.lua
@@ -1,12 +1,9 @@
 serial = require('serialization')
 filesystem = require('filesystem')
 component = require('component')
-io = require'io'
 tape = component.tape_drive
 
 fdb_size = 204800 -- size of the file descriptor block
-test_fdb = {}
-test_fdb['test.lua'] = { i = 204800, s = 50 }
 
 function tape.go(pos)
     return tape.seek(pos - tape.getPosition())
@@ -14,9 +11,10 @@ end
 
 function read_fdb()
     tape.go(0)
+
     size = tape.read(8):match('(.-)\0')
     fdb = tape.read(fdb_size):match('(.-)\0')
-    if size == '' then size = 0 end
+
     if fdb == '' then --- setup basic (empty) file descriptor block
         fdb = {}
         fdb['files'] = {}
@@ -25,6 +23,8 @@ function read_fdb()
     else
         fdb = serial.unserialize(fdb)
     end
+
+    if size == '' then size = 0 end
 
     return fdb, tonumber(size)
 end
@@ -49,22 +49,22 @@ end
 
 -------------------------------------------
 
-fdb, fdb_size = read_fdb()
+fdb, size = read_fdb()
 
-print("The file descriiptor block is:\n" .. serial.serialize(fdb) .. "\n")
+-- print("The file descriiptor block is:\n" .. serial.serialize(fdb) .. "\n")
 
-args = { ... }
+-- args = { ... or nil }
 
-if args[1] == 'put' then
-        -- file = shell.resolve(arg[2])
-        -- if filesystem.exists(file) then
-        files = {table.unpack( args, 2 )}
-        for i,file in ipairs(files) do
-            file = shell.resolve(file)
-            if filesystem.exists(file) then
-                put_file(file)
-            else
-                print('warning: file \"' .. file .. '\" doesn\'t exist' )
-            end
-        end
-end
+-- if args[1] == 'put' then
+--         -- file = shell.resolve(arg[2])
+--         -- if filesystem.exists(file) then
+--         files = {table.unpack( args, 2 )}
+--         for i,file in ipairs(files) do
+--             file = shell.resolve(file)
+--             if filesystem.exists(file) then
+--                 put_file(file)
+--             else
+--                 print('warning: file \"' .. file .. '\" doesn\'t exist' )
+--             end
+--         end
+-- end
